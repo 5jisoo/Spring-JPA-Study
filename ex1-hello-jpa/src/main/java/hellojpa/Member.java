e91c21f19ab9e3bdd96d2538a90e2bbef592d720
5jisoo <56earls@gmail.com>
2022-11-10 01:30:32 +0900

    6-3_일대일_양방향
    Member-Locker 일대일 양방향 코드 정리

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -17,6 +17,10 @@ public class Member {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)  // 읽기 전용으로 만들어 버리는 것!
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
