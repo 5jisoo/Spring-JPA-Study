775b4a8ebe1251642f1229748139a01c8c30daf9
5jisoo <56earls@gmail.com>
2023-01-06 18:25:42 +0900

    Section 10-2. 기본 문법과 쿼리 API _ 마무리

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -20,7 +17,22 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            Member result = em.createQuery("select m From Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result.getUsername());
+
+//            // 결과가 여러개인 경우
+//            List<Member> resultList = query.getResultList();
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+            Query query1 = em.createQuery("select m.username, m.age from Member m");            // 반환 타입이 명확하지 않음.
+
 
             tx.commit();
         } catch (Exception e) {
