f32af89419ff089a3f7a430567acaa0b9d14f964
5jisoo <56earls@gmail.com>
2023-03-01 14:03:24 +0900

    spring data JPA

--- jpashop2/src/main/java/jpabook/jpashop2/service/MemberService.java
+++ jpashop2/src/main/java/jpabook/jpashop2/service/MemberService.java
@@ -2,9 +2,7 @@ package jpabook.jpashop2.service;
 
 import jpabook.jpashop2.domain.Member;
 import jpabook.jpashop2.repository.MemberRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,12 +44,12 @@ public class MemberService {
      * 회원 조회
      */
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);   // 변경감지
     }
 }
--- jpashop2/src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
+++ jpashop2/src/test/java/jpabook/jpashop2/service/MemberServiceTest.java
@@ -2,12 +2,10 @@ package jpabook.jpashop2.service;
 
 import jpabook.jpashop2.domain.Member;
 import jpabook.jpashop2.repository.MemberRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
